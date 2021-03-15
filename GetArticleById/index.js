b979d41cda9594746ab16d852e3d20e67b8dcde6
hinocato <hinocato@gmail.com>
2021-03-16 05:32:29 +0900

    change database "dynamodb -> rds for postgresql"

--- GetArticleById/index.js
+++ GetArticleById/index.js
@@ -1,23 +1,86 @@
-const AWS = require('aws-sdk');
-const dynamoDBClient = new AWS.DynamoDB.DocumentClient({
-   region: "ap-northeast-1" 
+const { Client } = require('pg');
+const client = new Client({
+	user: process.env.USER,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT
 });
+const TABLE_NAME = process.env.TABLE_NAME;
 
-exports.handler = async (event, context) => {
-    const id = event.pathParameters.id - 0;
-    const params = {
-        TableName: 'vue-articles',
-        Key: { 
-            id,
-        }
-    };
+class Response {
+	constructor(statusCode, isBase64Encoded, body, headers) {
+		this.statusCode = statusCode;
+		this.isBase64Encoded = isBase64Encoded;
+		this.body = JSON.stringify(body);
+		this.headers = headers
+	}
+}
 
-    const response = {
-        statusCode: 200,
-        isBase64Encoded: false,
-        body: JSON.stringify(await dynamoDBClient.get(params).promise()),
-        headers: {},
-    };
-    
-    return response;
-};
+/* 簡単なバリデーションで実装 */
+const validate = function (event) {
+	if (!event || !event.pathParameters || !event.pathParameters.id) {
+		throw new Error('[pathParameters:id:notFound] Validatation Error');
+	}
+
+	if (!Number.isInteger(event.pathParameters.id - 0)) {
+		throw new Error('[pathParameters:id:notInteger] Validatation Error');
+	}
+}
+
+exports.handler = async function (event, context) {
+  console.log('connecting...');
+  try {
+		client.connect();
+	} catch (e) {
+		console.log(e);
+		const body = {
+			errorMessage: 'DB Connection Error' 
+		};
+		const response = new Response(400, false, body, {});
+		return response;
+	}
+
+	try {
+		validate(event);
+	} catch (e) {
+		console.log(e);
+		const body = {
+			errorMessage: e.message
+		};
+		const response = new Response(400, false, body, {});
+		return response;
+	}
+
+	const params = {
+		id: event.pathParameters.id,
+	};
+  
+	try {
+		const id = params.id;
+		const query = `SELECT * FROM ${TABLE_NAME} WHERE id = ${id}`;
+		const body = await new Promise((resolve, reject) => {
+			client.query(query, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				if (!res) {
+					reject(new Error(`No Response "${query}"`));
+				}
+				const result = res.rows[0];
+				resolve(result);
+			})
+		});
+		const response = new Response(200, false, body, {});
+		return response;
+	} catch (e) {
+		console.log(e);
+		const body = {
+			errorMessage: 'DB Query Error' 
+		};
+		const response = new Response(400, false, body, {});
+		return response;
+	} finally {
+		client.end();
+	}
+}
--- GetArticles/index.js
+++ GetArticles/index.js
@@ -1,34 +1,90 @@
-const AWS = require('aws-sdk');
-const dynamoDBClient = new AWS.DynamoDB.DocumentClient({
-   region: "ap-northeast-1" 
+const { Client } = require('pg');
+const client = new Client({
+	user: process.env.USER,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT
 });
+const TABLE_NAME = process.env.TABLE_NAME;
 
-exports.handler = async (event, context) => {
-    const id = event.pathParameters.id - 0;
-    const params = {
-        TableName: 'vue-articles',
-        Key: { 
-            id,
-        }
-    };
+class Response {
+	constructor(statusCode, isBase64Encoded, body, headers) {
+		this.statusCode = statusCode;
+		this.isBase64Encoded = isBase64Encoded;
+		this.body = JSON.stringify(body);
+		this.headers = headers
+	}
+}
 
-    const response = {
-        statusCode: 200,
-        isBase64Encoded: false,
-        body: JSON.stringify(await dynamoDBClient.get(params).promise()),
-        headers: {},
-    };
-    
-    return response;
-};
+/* 簡単なバリデーションで実装 */
+const validate = function (event) {
+	if (!event || !event.queryStringParameters || !event.queryStringParameters.page) {
+		throw new Error('[queryStringParameter:page:notFound] Validatation Error');
+	}
 
-const event = {
-    pathParameters: {
-        id: 1
-    }
-};
+	if (!event || !event.queryStringParameters || !event.queryStringParameters.size) {
+		throw new Error('[queryStringParameter:size:notFound] Validatation Error');
+	}
 
-new Promise(async (resolve, reject) => {
-    console.log(await exports.handler(event));
-    resolve();
-})
+	if (!Number.isInteger(event.queryStringParameters.page - 0)) {
+		throw new Error('[queryStringParameter:page:notInteger] Validatation Error');
+	}
+
+	if (!Number.isInteger(event.queryStringParameters.size - 0)) {
+		throw new Error('[queryStringParameter:size:notInteger] Validatation Error');
+	}
+
+	return {
+		size: event.queryStringParameters.size,
+		page: event.queryStringParameters.page,
+	}
+}
+
+exports.handler = async function (event, context) {
+  console.log('connecting...');
+  try {
+		client.connect();
+	} catch (e) {
+		console.log(e);
+		const body = {
+			errorMessage: 'DB Connection Error' 
+		};
+		const response = new Response(400, false, body, {});
+		return response;
+	}
+  
+	try {
+		const params = validate(event);
+		const limit = params.size;
+		const offset = params.size * (params.page - 1);
+		const query = `SELECT * FROM ${TABLE_NAME} limit ${limit} offset ${offset}`;
+		const body = await new Promise((resolve, reject) => {
+			client.query(query, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				if (!res) {
+					reject(new Error(`No Response "${query}"`));
+				}
+				const result = {
+					items: res.rows,
+					size: res.rowCount,
+					page: params.size
+				}
+				resolve(result);
+			})
+		});
+		const response = new Response(200, false, body, {});
+		return response;
+	} catch (e) {
+		console.log(e);
+		const body = {
+			errorMessage: 'DB Query Error' 
+		};
+		const response = new Response(400, false, body, {});
+		return response;
+	} finally {
+		client.end();
+	}
+}
