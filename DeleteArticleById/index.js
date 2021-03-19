ed1f60ee456e2a692200da9a5ca29644baf74f37
hinocato <hinocato@gmail.com>
2021-03-19 17:52:46 +0900

    add delete method

---
+++ DeleteArticleById/index.js
@@ -0,0 +1,75 @@
+const { Client } = require('pg');
+const client = new Client({
+	user: process.env.USER,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT
+});
+const TABLE_NAME = process.env.TABLE_NAME;
+
+class Response {
+	constructor(statusCode, isBase64Encoded, body, headers) {
+		this.statusCode = statusCode;
+		this.isBase64Encoded = isBase64Encoded;
+		this.body = JSON.stringify(body);
+		this.headers = headers
+	}
+}
+
+class ErrorBody {
+	constructor(errorMessage, errorCode) {
+		this.errorMessage = errorMessage;
+		this.errorCode = errorCode;
+	}
+}
+
+/* 簡単なバリデーションで実装 */
+const validate = function (event) {
+    const pathParameters = event.pathParameters;
+	if (!pathParameters || !Number.isInteger(pathParameters.id - 0)) {
+		throw new Error('[pathParameters:id:notInteger] Validatation Error');
+	}
+	return;
+}
+
+exports.handler = async function (event, context) {
+    try {
+		validate(event);
+	} catch (e) {
+		console.error(e);
+		const body = new ErrorBody('DB Validation Error', 4002);
+		const response = new Response(400, false, body, {"Access-Control-Allow-Origin": "*"});
+		return response;
+	}
+
+	const params = {
+		id: event.pathParameters.id,
+	};
+
+	try {
+		const queryString = `DELETE FROM ${TABLE_NAME} WHERE id = $1 AND is_saved = false RETURNING *`;
+		const values = [params.id];
+		const body = await new Promise((resolve, reject) => {
+			client.query(queryString, values, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+                if (!res.rows[0]) {
+                    reject(res)
+                }
+				resolve(res.rows[0]);
+			})
+		});
+		const response = new Response(200, false, body, {"Access-Control-Allow-Origin": "*"});
+		return response;
+	} catch (e) {
+		console.error(e);
+		const body = new ErrorBody('DB Query Error', 4004);
+		const response = new Response(400, false, body, {"Access-Control-Allow-Origin": "*"});
+		return response;
+	}
+}
+
+console.log('connecting...');
+client.connect();
